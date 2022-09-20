8ef7cc82fe6400e1832b4d98c22390236860fcb5
Lemonsprig <ianselbyuk@gmail.com>
2022-09-20 19:25:02 +0100

    Re-Adding files due to git corruption

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,11 @@
-
 let computerChoice = ""
 let playerChoice = ""
 let playerRoundsWon = 0
 let computerRoundsWon = 0
-
+const message = document.querySelector("#message");
+const options = document.querySelectorAll("img");
+const contentContainer = document.querySelector(".content-container");
+const playAgain = document.createElement("button");
 
 function getComputerChoice(){
     let num = Math.floor(Math.random()*3)+1;
@@ -18,8 +20,38 @@ function getComputerChoice(){
     }
 }
 
-function playRound(computerChoice, playerChoice) {
+function updateScore() {
+    const computer = document.querySelector("#computerScore");
+    const player = document.querySelector("#playerScore");
+
+    computer.textContent = computerRoundsWon;
+    player.textContent = playerRoundsWon;
+
+    if (computerRoundsWon == 5 || playerRoundsWon ==5) {
+        if (computerRoundsWon == 5) {
+            message.textContent = `Computer wins ${computerRoundsWon} rounds to ${playerRoundsWon}`;
+        } else {
+            message.textContent = `Player wins ${playerRoundsWon} rounds to ${computerRoundsWon}`;
+        }
+        gameOver();
+    }
+}
+
+function gameOver() {
+    console.log(options)
+    options.forEach(option => {option.removeEventListener("click", playRound)});
+    options.forEach(option => {option.removeEventListener("mouseenter", optionHover)});
+    options.forEach(option => {option.classList.remove("optionHover")});
+    options.forEach(option => {option.removeEventListener("mouseleave", removeOptionHover)});
+    playAgain.textContent = "Play again";
+    contentContainer.appendChild(playAgain);
+    playAgain.addEventListener("click", resetGame);
+}
+
+function playRound() {
     let outcome = ""
+    computerChoice = getComputerChoice()
+    playerChoice = this.id
     if (computerChoice === playerChoice) {
         outcome = `${playerChoice} vs ${computerChoice} a Tie!`
     }
@@ -33,29 +65,35 @@ function playRound(computerChoice, playerChoice) {
         outcome = `${playerChoice} loses to ${computerChoice}, computer wins!`
         computerRoundsWon ++
     }
-    return outcome;
+    console.log(outcome);
+    message.textContent = outcome
+    updateScore();
 }
 
-function game() {
-    computerChoice = getComputerChoice();
-    playerChoice = this.id //getPlayerChoice();
-    console.log(playRound(computerChoice, playerChoice));
+function optionHover() {
+    this.classList.add("optionHover");
+}
+
+function removeOptionHover() {
+    this.classList.remove("optionHover");
+}
+
+function resetGame() {
+    console.log("Reset");
+    playerRoundsWon = 0;
+    computerRoundsWon = 0;
+    message.textContent = "";
+    contentContainer.removeChild(contentContainer.lastChild);
+
+    updateScore();
     
-    if (playerRoundsWon > computerRoundsWon) {
-        return "Player Wins"
-    }
-    else {
-        return "Computer Wins"
-    }
+    options.forEach(option => {option.addEventListener("click", playRound)
+    });
+    options.forEach(option => {option.addEventListener("mouseenter", optionHover)
+    });
+    options.forEach(option => {option.addEventListener("mouseleave", removeOptionHover)
+    });
 }
 
-const options = document.querySelectorAll("img");
+resetGame()
 
-options.forEach(option => {option.addEventListener("click", game)
-    console.log("box clicked", option.id);
-    option.setAttribute("style", "border: 1px solid red");
-
-});
-
-
-console.log(options);
